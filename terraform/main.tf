5da52ce1cb558fb74e2b55c3d96d1150641514a6
Yancy <yency.christopher@gmail.com>
2026-05-04 11:49:36 -0400

    Changed public IP to Standark SKU

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,6 +43,7 @@ resource "azurerm_public_ip" "main" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  sku                 = "Standard" 
 }
 
 resource "azurerm_network_security_group" "nsg" {
